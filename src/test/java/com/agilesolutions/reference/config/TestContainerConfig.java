a88482d959308c2c7b8361e20bd12820e24929ab
admin <rong@agile-solutions.ch>
2026-02-04 14:14:42 +0100

    fix pipeline

--- src/test/java/com/agilesolutions/reference/config/TestContainerConfig.java
+++ src/test/java/com/agilesolutions/reference/config/TestContainerConfig.java
@@ -13,6 +13,4 @@ public class TestContainerConfig {
     public NamedParameterJdbcTemplate namedParameterJdbcTemplate(DataSource dataSource) {
         return new NamedParameterJdbcTemplate(dataSource);
     }
-
-
 }
